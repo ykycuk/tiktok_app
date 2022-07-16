48abbb64e8ab49840532cbe80c0e037dbbb1aa45
BatyrArtem <artembatyr88@gmail.com>
2022-07-16 16:36:14 +0300

    add function postion video

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -77,11 +77,11 @@ const onVideoPress = () => {
                 onMouseEnter={() => setIsHover(true)}
                 onMouseLeave={() => setIsHover(false)}
             >
-                <Link href="/">
+                <Link href={`/detail/${post._id}`}>
                     <video
                         loop
                         ref={videoRef}
-                        className="lg:w[600px] h-[300px]
+                        className="lg:w-[600px] h-[300px]
                         md:h-[400px] lg:h[530px] w-[200px]
                         rounded-2xl cursor-pointer bg-gray-100"
                         src={post.video.asset.url}
