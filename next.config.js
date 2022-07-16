a48d91f38476c21ae2f9e746905958f0db514e5e
BatyrArtem <artembatyr88@gmail.com>
2022-07-16 12:54:51 +0300

    add google oauth

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['www.yamanashi-kankou.jp']
+    domains: [
+      'raw.githubusercontent.com',
+      "lh3.googleusercontent.com"
+  ]
   }
 }
 
